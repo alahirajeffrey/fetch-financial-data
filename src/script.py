f685a56b229ec06bd21556d1bc5c3eecf926d2dd
AlahiraJ <alahirajeffrey@gmail.com>
2022-03-25 23:10:54 +0100

    edited script

--- src/script.py
+++ src/script.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import pandas as pd
 
 # get user inputs
 TICKER = input("Enter ticker : ")
@@ -10,7 +11,10 @@ END = input("Enter end date : ")
 
 def fetch_data(ticker, start, end):
     data = yf.download(ticker, start=start, end=end)
-    return data
+
+    # save to csv file
+    data.to_csv(f'{ticker}.csv', encoding='utf-8')
+    print("Data stored in csv file!!")
 
 
 if __name__ == "__main__":
